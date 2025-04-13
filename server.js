06d02011c8d15d65cd38965a731ab16fab564fc4
Dimitri <dimitri.remez@ravelin.com>
2025-04-13 13:40:40 +0200

    Fix: use live Render URL for Flow redirect

--- server.js
+++ server.js
@@ -60,8 +60,8 @@ app.post("/create-payment-sessions", async (_req, res) => {
         risk: {
           enabled: true,
         },
-        success_url: "http://localhost:3000/?status=succeeded",
-        failure_url: "http://localhost:3000/?status=failed",
+        success_url: "https://cko-flow.onrender.com/?status=succeeded",
+        failure_url: "https://cko-flow.onrender.com/?status=failed",
         metadata: {},
         items: [
           {
